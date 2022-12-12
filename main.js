354eee4f4b380cf5ece4ab3314ae2c0167ab9a9d
Jenny Kitchen <jennykayekitchen@gmail.com>
2022-12-12 09:51:51 -0600

    project upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const fillGasTank = (gallons) => {
+    if (gallons <= 15){
+        console.log(`I filled the tank with ${gallons} of gas`)
+    } else {
+        console.log("You cannot add this much gas to the tank.")
+    }
+}
+fillGasTank(21);
+
